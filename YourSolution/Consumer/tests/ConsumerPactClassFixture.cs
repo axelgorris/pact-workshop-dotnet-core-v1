bb1c9f5e4634edc111389bd6af1d0addd8839bd6
Axel Gorris <axelgd@outlook.com>
2019-04-22 23:21:55 +0200

    fixup! WIP

--- YourSolution/Consumer/tests/ConsumerPactClassFixture.cs
+++ YourSolution/Consumer/tests/ConsumerPactClassFixture.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using PactNet;
+using PactNet.Mocks.MockHttpService;
 
 namespace tests
 {
@@ -9,5 +11,53 @@ namespace tests
     // See: https://goo.gl/hSq4nv
     public class ConsumerPactClassFixture
     {
+        public IPactBuilder PactBuilder { get; private set; }
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public int MockServerPort { get { return 9222; } }
+        public string MockProviderServiceBaseUri { get { return String.Format("http://localhost:{0}", MockServerPort); } }
+
+        public ConsumerPactClassFixture()
+        {
+            // Using Spec version 2.0.0 more details at https://goo.gl/UrBSRc
+            var pactConfig = new PactConfig
+            {
+                SpecificationVersion = "2.0.0",
+                PactDir = @"..\..\..\..\..\pacts",
+                LogDir = @".\pact_logs"
+            };
+
+            PactBuilder = new PactBuilder(pactConfig);
+
+            PactBuilder.ServiceConsumer("Consumer")
+                       .HasPactWith("Provider");
+
+            MockProviderService = PactBuilder.MockService(MockServerPort);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // This will save the pact file once finished.
+                    PactBuilder.Build();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
     }
 }
